7df45c3914be275b4e5271d8c14983daad2fdd03
WellingtonSI <12ribe@gmail.com>
2023-07-10 11:58:25 -0300

    adicação de um novo scraping

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\WebScrapingController;
+use App\Http\Controllers\ScrapingDaninnController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
 Route::get('/web-scraping', [WebScrapingController::class, 'webScraping']);
+Route::get('/scraping-daninn', [ScrapingDaninnController::class, 'scrapingDaninn']);
